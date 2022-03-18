5b23c7200653d91c23c17264454222979aa68de1
maximus3 <max2000.gym5cheb@yandex.ru>
2022-03-18 22:06:39 +0300

    add_new_theme

--- database/views.py
+++ database/views.py
@@ -1,9 +1,20 @@
+import peewee
+
 from status_code import StatusCode
+from models import AllThemes
 
 
-def add_new_theme(theme_name):  # TODO
-    print(theme_name)
-    return StatusCode.OK, None
-    # data = AllThemes.select().where(AllThemes.theme_name == theme_name).get()
-    # breakpoint()
-    # return StatusCode.OK
+def add_new_theme(theme_name, tg_bot_token, bot_nick):  # TODO
+    if AllThemes.select().where(AllThemes.theme_name == theme_name).count() > 0:
+        return StatusCode.THEME_ALREADY_EXISTS, None
+    theme = None
+    try:
+        theme = AllThemes.create(theme_name=theme_name, tg_bot_token=tg_bot_token, bot_nick=bot_nick)
+    except peewee.IntegrityError as exc:
+        if str(exc).startswith('UNIQUE'):
+            return StatusCode.THEME_ALREADY_EXISTS, None
+        else:
+            return StatusCode.ERROR_IN_ADD_TO_DATABASE, None
+    except Exception:
+        return StatusCode.ERROR_IN_ADD_TO_DATABASE, None
+    return StatusCode.OK, theme
