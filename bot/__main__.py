a0916273a2a4e7060e48a7651e953f5c0ca343f2
maximus3 <max2000.gym5cheb@yandex.ru>
2022-03-19 17:13:35 +0300

    new bot scheme

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,10 +6,10 @@ from config import BOT_FATHER_ID, MAIN_BOT_API_HASH, MAIN_BOT_API_ID
 
 from .utils import (
     get_add_message,
-    get_list_message,
-    get_reg_message,
-    get_username,
+    user_is_bot
 )
+
+from .text_data import HELLO_MESSAGE
 
 api_id = MAIN_BOT_API_ID
 api_hash = MAIN_BOT_API_HASH
@@ -19,42 +19,28 @@ logger = logging.getLogger(__name__)
 
 @app.on_message(filters.command('start') & filters.private)  # type: ignore
 async def handler_start(_: Client, message: types.Message) -> None:
-    await message.reply(
-        """Привет!
-Напиши `/list` для просмотра всех существующих тем
-Напиши `/add` для добавления новой темы"""
-    )
-
-
-@app.on_message(filters.command('list') & filters.private)  # type: ignore
-async def handler_list(_: Client, message: types.Message) -> None:
-    text_to_send = get_list_message()
-    await message.reply(text_to_send)
+    await message.reply(HELLO_MESSAGE)
 
 
 @app.on_message(filters.command('add') & filters.private)  # type: ignore
 async def handler_add(client: Client, message: types.Message) -> None:
-    await message.reply('Подождите, идет процесс создания бота')
+    await message.reply('Подождите, идет процесс создания чата')
     text_to_send = await get_add_message(client, message.text)
-    await message.reply(text_to_send)
-
-
-@app.on_message(filters.command('reg') & filters.private)  # type: ignore
-async def handler_reg(_: Client, message: types.Message) -> None:
-    text_to_send = get_reg_message(message.text, message)
     await message.reply(text_to_send)
 
 
 @app.on_message(filters.text & filters.private)  # type: ignore
 async def handler_text(_: Client, message: types.Message) -> None:
-    username = get_username(message)
-    if '@' + username == BOT_FATHER_ID:
+    if user_is_bot(message):
         return
-    await message.reply(
-        """Привет!
-Напиши `/list` для просмотра всех существующих тем
-Напиши `/add` для добавления новой темы"""
-    )
+    await message.reply(HELLO_MESSAGE)
+
+
+# @app.on_message(filters.group_chat_created)
+# async def text(client, message):
+#     theme_name = message.chat.title
+#     await message.reply(f'''Тема {theme_name} создана
+# Пришлите начальное сообщение для темы''')
 
 
 app.run()
--- bot/utils.py
+++ bot/utils.py
@@ -4,85 +4,70 @@ from typing import Tuple
 from pyrogram import Client, types
 
 import database.views
-from config import SECRET_KEY
 
-from .new_bot_registration import register_new_bot
 from .status_code import StatusCode
 
 logger = logging.getLogger(__name__)
 
 
-def start_new_chat(tg_bot_token: str) -> StatusCode:  # TODO
-    logger.info('Start new bot with token=%s', tg_bot_token)
-    return StatusCode.OK
+async def create_new_chat(client: Client, theme_name: str, user_id: str, start_message: str) -> Tuple[StatusCode, int, str]:  # TODO
+    with client:
+        chat = await client.create_group(theme_name, user_id)
+        await client.send_message(chat.id, start_message)
+        invite_link = await client.create_chat_invite_link(chat.id, member_limit=1)
+    return StatusCode.OK, chat.id, invite_link.invite_link
 
 
-def check_message_add(text: str) -> Tuple[StatusCode, str]:  # TODO: validate
-    splitted_text = text.split()
+def check_message_add(text: str) -> Tuple[StatusCode, str, str]:  # TODO: validate
+    splitted_text = text.split('\n')
     if len(splitted_text) < 2:
-        return StatusCode.WRONG_PARAMETERS_COUNT, ''
-    return StatusCode.OK, ' '.join(splitted_text[1:])
+        return StatusCode.WRONG_PARAMETERS_COUNT, '', ''
+    com_and_theme = splitted_text[0].split()
+    start_message = '\n'.join(splitted_text[1:])
+    if len(com_and_theme) < 2:
+        return StatusCode.WRONG_PARAMETERS_COUNT, '', ''
+    command, theme = com_and_theme[:1], ' '.join(com_and_theme[1:])
+    return StatusCode.OK, theme, start_message
 
 
 async def add_new_theme(
-    theme_name: str, client: Client
+    theme_name: str, start_message: str, client: Client, user_id: str
 ) -> Tuple[StatusCode, str]:
-    status_check_db_add = database.views.check_new_theme(theme_name)
-    if status_check_db_add:
-        logger.error(
-            'Bot by theme=%s error: %s', theme_name, status_check_db_add
-        )
-        return StatusCode.ERROR_THEME_ALREADY_EXISTS, ''
 
-    status_register, tg_bot_token, bot_nick = await register_new_bot(
-        theme_name, client
+    status_create, chat_id, invite_link = await create_new_chat(
+        client, theme_name, user_id, start_message
     )
-    if status_register:
-        logger.error('Bot by theme=%s error: %s', theme_name, status_register)
+    if status_create:
+        logger.error('Chat by theme=%s error: %s', theme_name, status_create)
         return StatusCode.ERROR_IN_REGISTER_NEW_BOT, ''
     logger.info(
-        'Bot by theme=%s created with username=%s', theme_name, bot_nick
+        'Chat by theme=%s created with chat_id=%s', theme_name, chat_id
     )
 
     status_db_add = database.views.add_new_theme(
-        theme_name, tg_bot_token, bot_nick
+        theme_name, user_id, chat_id, start_message
     )
     if status_db_add:
-        logger.error('Bot by theme=%s error: %s', theme_name, status_db_add)
+        logger.error('Chat by theme=%s error: %s', theme_name, status_db_add)
         return StatusCode.ERROR_IN_ADD_TO_DATABASE, ''
-    logger.info('Bot by theme=%s db created', theme_name)
+    logger.info('Chat by theme=%s db created', theme_name)
 
-    status_run_bot = start_new_chat(tg_bot_token)
-    if status_run_bot:
-        logger.error(
-            'Bot by theme=%s start error: %s', theme_name, status_run_bot
-        )
-        return StatusCode.RUN_BOT_ERROR, ''
-    logger.info('Bot by theme=%s run', theme_name)
-
-    return StatusCode.OK, bot_nick
+    return StatusCode.OK, invite_link
 
 
-def get_list_message() -> str:
-    text = 'Список всех тем:\n'
-    themes_list = database.views.get_themes_list()
-    for theme_name, bot_nick in themes_list:
-        text += f'{theme_name}\t@{bot_nick}\n'
-    return text
-
-
-async def get_add_message(client: Client, text: str) -> str:
-    status, theme_name = check_message_add(text)
+async def get_add_message(client: Client, message: types.Message, text: str) -> str:
+    status, theme_name, start_text = check_message_add(text)
+    user_id = get_user_id(message)
     if status:
         logger.error('check_message_add error: %s', status)
         return 'Error'  # TODO
-    status, bot_nick = await add_new_theme(theme_name, client)
+    status, invite_link = await add_new_theme(theme_name, start_text, client, user_id)
     if status:
         logger.error('add_new_theme error: %s', status)
         if status == StatusCode.ERROR_THEME_ALREADY_EXISTS:
             return 'Такая тема уже есть'
         return 'Error'  # TODO
-    return f'@{bot_nick}'  # TODO
+    return f'{invite_link}'  # TODO
 
 
 def get_username(message: types.Message) -> str:
@@ -93,16 +78,10 @@ def get_user_id(message: types.Message) -> str:
     return str(message.from_user.id)
 
 
-def get_reg_message(text: str, message: types.Message) -> str:
-    splitted_text = text.split()
-    if len(splitted_text) != 2:
-        return 'Error format'  # TODO
-    user_tg_id, username = get_user_id(message), get_username(message)
-    if splitted_text[1] == SECRET_KEY:
-        logger.info('Add admin %s %s', user_tg_id, username)
-        if database.views.add_admin(user_tg_id, username):
-            return 'Ok'  # TODO
-        return 'Error db'  # TODO
+def user_is_bot(message: types.Message):
+    return message.from_user.is_bot
 
-    logger.info('%s wrong code', user_tg_id)
-    return 'Error code'
+def random_word(length: int) -> str:
+    return ''.join(
+        random.choice(string.ascii_lowercase) for i in range(length)
+    )
