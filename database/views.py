aec16e78ed66e64e85f1fb1c813eb29f0c139860
maximus3 <max2000.gym5cheb@yandex.ru>
2022-03-19 13:39:05 +0300

    get_themes_list, add_admin, user_is_admin

--- database/views.py
+++ database/views.py
@@ -2,13 +2,13 @@ from typing import List, Tuple
 
 import peewee
 
-from .models import AllThemes
+from .models import AllThemes, User
 from .status_code import StatusCode
 
 
 def add_new_theme(
     theme_name: str, tg_bot_token: str, bot_nick: str
-) -> StatusCode:  # TODO
+) -> StatusCode:
     if (
         AllThemes.select().where(AllThemes.theme_name == theme_name).count()
         > 0
@@ -25,10 +25,25 @@ def add_new_theme(
     return StatusCode.OK
 
 
-def get_themes_list() -> List[Tuple[str, str]]:  # TODO
-    return []
+def get_themes_list() -> List[Tuple[str, str]]:
+    result = []
+    for theme in AllThemes.select():
+        result.append((theme.theme_name, theme.bot_nick))
+    return result
 
 
-def add_admin(user_tg_id: str, username: str) -> bool:  # TODO
-    print(user_tg_id, username)
-    return False
+def add_admin(user_tg_id: str, username: str) -> bool:
+    if User.select().where(User.user_tg_id == user_tg_id).count() > 0:
+        user = User.select().where(User.user_tg_id == user_tg_id).get()
+        user.is_admin = True
+        user.save()
+        return True
+    User.create(user_tg_id=user_tg_id, username=username, is_admin=True)
+    return True
+
+
+def user_is_admin(user_tg_id: str) -> bool:
+    if User.select().where(User.user_tg_id == user_tg_id).count() == 0:
+        return False
+    user = User.select().where(User.user_tg_id == user_tg_id).get()
+    return user.is_admin
